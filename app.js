26ae7135839c7a125f4ae35d7aae1cb0382d2fb1
JungUk <62638035+JungUk@users.noreply.github.com>
2022-02-25 21:44:53 +0900

    2.25

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("hi");
+const a = 5;
+const b = 3;
+
+let myName = "Lincoln";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+
+console.log("hello" + myName);
+
+myName = "nico";
+
+console.log("hello" + myName);
