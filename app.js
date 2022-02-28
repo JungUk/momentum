54ec6b6559cbc3075cfa77e23feea69c5dfb2dff
JungUk <62638035+JungUk@users.noreply.github.com>
2022-02-28 20:10:00 +0900

    2.28 3rd

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 
-function sayHello() {
-  console.log("Hello! C");
+function sayHello(nameOfPerson, age) {
+  console.log("Hello I'm " + nameOfPerson + ". " + age);
 }
 
-sayHello();
-sayHello();
-sayHello("nico");
+sayHello("dal", 10);
+sayHello("lynn", 20);
+sayHello("nico", 30);
+
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+
+plus(1, 2);
+divide(10, 2);
+
+
+const player = {
+  name: "nico",
+  sayhello: function(PersonName) {
+    console.log("Hello " + PersonName);
+  }
+};
+
+console.log(player.name);
+player.sayhello("Cola");
