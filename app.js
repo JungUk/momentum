c9c4c0f236f9eaf87a039c2053c9df9222ac1e65
JungUk <62638035+JungUk@users.noreply.github.com>
2022-02-28 19:45:16 +0900

    2.28

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const amIFat = false;
 
-console.log(amIFat);
+
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+player.fat = false;
+player.lastName = "potato";
+player.points = 15;
+player.points = player.points + 15;
+console.log(player);
