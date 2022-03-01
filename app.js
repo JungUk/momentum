42caca5952dc253112a3b9b428b7af6946feed19
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-01 23:55:18 +0900

    3.1

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-const age = 96;
-function calculateKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
-}
+const age = prompt("How old are you?");
 
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
-
-
-const calculator = {
-  plus: function(a, b) {
-    return a + b;
-  },
-  minus: function(a, b) {
-    return a - b;
-  }
-};
-
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 2);
-console.log(minusResult);
+console.log(age);
+console.log(typeof "15", typeof parseInt("15"));
