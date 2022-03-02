18f237ce4fc737a3f34c04adf0d3987e929ee052
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-02 17:03:42 +0900

    3.2

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const age = prompt("How old are you?");
 
-console.log(age);
-console.log("age의 타입 " + typeof age);
+// console.log(age);
+// console.log("age의 타입 " + typeof age);
 
-const agee = parseInt(prompt("HOAU"));
-console.log(agee);
-console.log("agee의 타입 " + typeof agee);
+// const agee = parseInt(prompt("HOAU"));
+// console.log(agee);
+// console.log("agee의 타입 " + typeof agee);
+
+if(isNaN(age)) {
+  console.log("Please write a number");
+}
+else {
+  console.log("Thank you for writing your age.");
+}
