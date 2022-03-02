afdc7dc5fe1aad1d9974db28555b60c8c2661070
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-02 17:49:40 +0900

    3.2

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const age = prompt("How old are you?");
 // console.log(agee);
 // console.log("agee의 타입 " + typeof agee);
 
-if(isNaN(age)) {
+if(isNaN(age) || age < 0) {
   console.log("Please write a number");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink.");
 }
 else {
   console.log("Thank you for writing your age.");
