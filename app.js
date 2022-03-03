bf231fd7aa2d9a799c825d6d80b875286e4a1e22
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-03 17:53:10 +0900

    3.3

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-const age = prompt("How old are you?");
+document.title = "Hello! from JS";
 
-// console.log(age);
-// console.log("age의 타입 " + typeof age);
+const title = document.getElementById("title");
 
-// const agee = parseInt(prompt("HOAU"));
-// console.log(agee);
-// console.log("agee의 타입 " + typeof agee);
+title.innerText = "Got you!";
 
-if(isNaN(age) || age < 0) {
-  console.log("Please write a number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink.");
-}
-else {
-  console.log("Thank you for writing your age.");
-}
+console.log(title.id);
+console.log(title.className);
