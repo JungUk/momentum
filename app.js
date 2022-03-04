ab336fe886839f06a269481f6c08e20a58df62b2
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-04 22:38:42 +0900

    3.4

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const title = document.querySelectorAll(".hello h1");
+const title = document.querySelector(".hello:first-child h1");
 
 console.log(title);
+
+
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
+
+// addEventListener??? googling
