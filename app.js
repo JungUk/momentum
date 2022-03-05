e4e3b7b706ae9577eee6e2dbebc4ed0aaac5d585
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-05 23:57:48 +0900

    3.5

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ const title = document.querySelector(".hello:first-child h1");
 console.log(title);
 
 
+// function handleTitleClick() {
+//   if(title.style.color != black) {
+//   title.style.color = "blue"; }
+// }
+
+// function handleTitleClickBlack() {
+//   if(title.style.color == blue) {
+//   title.style.color = "black"; }
+// }
+
 function handleTitleClick() {
-  title.style.color = "blue";
+    title.style.color = "blue";
+  }
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
 }
 
 title.addEventListener("click", handleTitleClick);
-
+title.addEventListener("click", handleTitleClickBlack);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
 // addEventListener??? googling
