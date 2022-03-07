0e40267530d7336ab118e17514f4e693c3697c49
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-07 20:26:23 +0900

    3.7

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  const correntColor = h1.style.color;
-  let newColor
-
-  if(correntColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-
-  h1.style.color = newColor;
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
