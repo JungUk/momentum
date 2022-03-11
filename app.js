b6d659b938a9503300a1b49258c2c63f976042b6
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-12 01:59:34 +0900

    3.12

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
   console.log(username);
-  localStorage.setItem("username", username);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem("USERNAME_KEY", username);
+  paintGreetings(username);
 }
 
 function onLinkClick(event) {
@@ -21,4 +21,16 @@ function onLinkClick(event) {
   // alert("clicked!");
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if(savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
