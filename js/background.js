b5959978811b2e897c145027e568c2c695ea5e53
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-17 00:28:19 +0900

    3.17

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
+
+const todayImage = images[Math.floor(Math.random() * images.length)];
+
+const bgimage = document.createElement("img");
+
+bgimage.src = `img/${todayImage}`;
+
+console.log(bgimage);
+
+document.body.appendChild(bgimage);
