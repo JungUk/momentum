c17cda0978018dc467714d794b90e4be6b030b72
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-16 23:49:54 +0900

    3.16

--- js/quotes.js
+++ js/quotes.js
@@ -7,21 +7,12 @@ const quotes = [
     author: "칼 바르트" },
   { quote: "나의 대적이여 나로 말미암아 기뻐하지 말지어다 나는 엎드러질지라도 일어날 것이요 ",
     author: "미가" },
-  { quote: "어두운 데에 앉을지라도 여호와께서 나의 빛이 되실 것임이로다 ",
-    author: "미가" },
-  { quote: "한 손에는 성경을 다른 한 손엔 신문을 들고 ", 
-    author: "칼 바르트" },
-  { quote: "나의 대적이여 나로 말미암아 기뻐하지 말지어다 나는 엎드러질지라도 일어날 것이요 ",
-    author: "미가" },
-  { quote: "어두운 데에 앉을지라도 여호와께서 나의 빛이 되실 것임이로다 ",
-    author: "미가" },
-  { quote: "한 손에는 성경을 다른 한 손엔 신문을 들고 ", 
-    author: "칼 바르트" },
-  { quote: "한 손에는 성경을 다른 한 손엔 신문을 들고 ", 
-    author: "칼 바르트" }
 ];
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-console.log(quotes[Math.floor(Math.random() * 10)]);
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todayQuote.quote;
+author.innerText = todayQuote.author;
