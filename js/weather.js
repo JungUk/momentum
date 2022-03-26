6a89251ce58dbde4e989ebd4a24a63640cc60ccd
JungUk <62638035+JungUk@users.noreply.github.com>
2022-03-26 19:07:38 +0900

    3.26

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,21 @@
+
+const API_KEY = "98cb267c00e002a7b05e19f617057513";
+
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  console.log(url);
+  fetch(url)
+  .then(response => response.json())
+  .then(data => 
+    {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
